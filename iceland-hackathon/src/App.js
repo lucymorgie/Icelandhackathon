50c5f76120d92a5520ff8f6248c8f43d7aea999b
Coding-Abdi <abdi.brum@gmail.com>
2023-08-09 12:00:56 +0100

    header and mission statement created

--- iceland-hackathon/src/App.js
+++ iceland-hackathon/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/header';
+import Mission from './components/Mission/mission';
 
 function App() {
   return (
     <div className="App">
-     <Deliveryoptions/>
-     <Scoretotals/>
+     {/* <Deliveryoptions/> */}
+     {/* <Scoretotals/> */}
      <Header/>
+     <Mission/>
 
     </div>
   );
---
+++ iceland-hackathon/src/components/Mission/mission.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './mission.css';
+
+const Mission = () => {
+    return (
+        <div className="mission">
+            <h1>Mission Statement</h1>
+        </div>
+    )
+}
+
+export default Mission;
