73be1e9e02cb5c790fd20f807c14bb60bc527340
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2023-08-09 14:46:31 +0100

    stlyed footer

--- iceland-hackathon/src/App.js
+++ iceland-hackathon/src/App.js
@@ -2,27 +2,32 @@ import "./App.css";
 import React, { useState } from "react";
 import Deliveryoptions from "./components/Deliveryoptions";
 import Scoretotals from "./components/Scoretotals";
-import Header from './components/Header/header';
-import Mission from './components/Mission/mission';
-import ProductTile from './components/ProductTile/ProductTile';
+import Header from "./components/Header/header";
+import Mission from "./components/Mission/mission";
+import ProductTile from "./components/ProductTile/ProductTile";
 
 function App() {
-const [scoretotals, setScoretotals] = useState(0);
-const [deliveryscore, setdeliveryscore] = useState(0);
+  const [scoretotals, setScoretotals] = useState(0);
+  const [deliveryscore, setdeliveryscore] = useState(0);
 
   return (
     <div className="App">
       <Header />
-      <div className="shopfront">
-        
+      <div className="shopfront"></div>
+      <Mission />
+      <ProductTile setScoretotals={setScoretotals} scoretotals={scoretotals} />
+      <div className="stickyfooter">
+      <div className="deliveryoptions">
+      <Deliveryoptions
+        deliveryscore={deliveryscore}
+        setdeliveryscore={setdeliveryscore}
+      />
       </div>
-     <Mission/>
-     <ProductTile setScoretotals={setScoretotals} scoretotals={scoretotals}/>
-     <Deliveryoptions deliveryscore={deliveryscore} setdeliveryscore={setdeliveryscore}/>
-        <Scoretotals scoretotals={scoretotals} deliveryscore={deliveryscore}/>
-
+      <div className="score">
+        <Scoretotals scoretotals={scoretotals} deliveryscore={deliveryscore} />
+        </div>
+      </div>
     </div>
-    
   );
 }
 
